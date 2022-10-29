98e411ece7c243004786dafc73c38c87920355f0
Phước Trường <phuoctruong554@gmail.com>
2022-10-29 08:32:09 +0700

    123

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 				appId="441659350368423"
 			/> */}
 			<MessengerCustomerChat
-				pageId="106761812238913"
+				pageId="100086242666455"
 				appId="2960111497624310"
 			/>
 		</div>
