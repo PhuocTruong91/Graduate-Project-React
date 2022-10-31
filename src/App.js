84da4bf0d6ac00d5eb0b58b9e02a54fd00255f01
Phuoc Truong <phuoctruong554@gmail.com>
2022-10-31 21:19:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -19,18 +19,6 @@ function App() {
 				</Routes>
 			</BrowserRouter>
 			<IndexPopup></IndexPopup>
-			{/* <MessengerCustomerChat
-				pageId="106761812238913"
-				appId="2508642169281267"
-			/> */}
-			{/* <MessengerCustomerChat
-				pageId="100086242666455"
-				appId="441659350368423"
-			/> */}
-			<MessengerCustomerChat
-				pageId="106172958935015"
-				appId="2960111497624310"
-			/>
 		</div>
   	);
 }
